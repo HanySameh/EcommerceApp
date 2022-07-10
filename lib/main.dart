8ee4a2b70bb0374002d8fda88cf817646d29fcc9
Hany Sameh <hanysameh@Hanys-MacBook-Pro.local>
2022-07-10 04:29:22 +0200

    Auth UI

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Ecommerce',
       theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFE5E5E5),
         primaryColor: Colors.red,
+        inputDecorationTheme: InputDecorationTheme(
+          labelStyle: Theme.of(context).textTheme.subtitle1,
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5.0),
+            borderSide: const BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5.0),
+            borderSide: const BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5.0),
+            borderSide: const BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+        ),
       ),
       onGenerateRoute: onGenerate,
       initialRoute: AppRoutes.loginPageRoute,
